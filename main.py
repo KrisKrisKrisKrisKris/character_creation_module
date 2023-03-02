42026c529b016f3622e7bc18673cd743b074aea0
Kris <kris.eretikova@yandex.ru>
2023-03-02 13:44:44 +0300

    Save before fork and clone calc_and_win

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция активации атаки для всех персонажей."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 '{5 + randint(3, 5)}')
@@ -17,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция активации защиты для всех персонажей."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -27,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция активации специального умения для всех персонажей."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 '«Выносливость {80 + 25}»')
@@ -38,6 +41,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция активации начальной трентровки для всех перснажей."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +66,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора персонажа и описание его свойств."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -86,6 +91,7 @@ def choice_char_class() -> str:
 
 if __name__ == '__main__':
     run_screensaver()
+    """Функция активации приветствия в начале игры."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
